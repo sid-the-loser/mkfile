298038e164524f48f374d01557f49c0968c7a83c
sid-the-loser <sidharthsuresh07@gmail.com>
2025-04-25 06:47:58 +0400

    added ommit flag incase somebody doesn't want their absolute path to the file and username being exposed

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ parser = argparse.ArgumentParser(prog="mkfile",
 
 parser.add_argument('filename', 
                     help="name of the file you want to create")
+parser.add_argument("-o", "--ommit", action="store_true", help="ommits the absolute path being printed out into the command line")
 
 args = parser.parse_args()
 
 filename = args.filename
+ommit_flag = args.ommit
 
 if not os.path.exists(filename):
     open(filename, "wb")
-    print(os.path.abspath(filename), "created!")
+    print(os.path.basename(filename) if ommit_flag else os.path.abspath(filename), "created!")
 
 else:
-    print(os.path.abspath(filename), "already exists!") # create a custom error here
-
+    print(os.path.basename(filename) if ommit_flag else os.path.abspath(filename), "already exists!") # create a custom error here
