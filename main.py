6028b9e30a5024426cc8551df959db6b3a1f0126
sid-the-loser <sidharthsuresh07@gmail.com>
2025-04-25 06:00:51 +0400

    simple implementation

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import argparse
+import os
 
+parser = argparse.ArgumentParser(prog="mkfile",
+                                 description="A command that makes files.",
+                                 epilog="Command made by SidTheLoser")
+
+parser.add_argument('filename', 
+                    help="name of the file you want to create")
+
+args = parser.parse_args()
+
+filename = args.filename
+
+if not os.path.exists(filename):
+    open(filename, "wb")
+    print(os.path.abspath(filename), "created!")
+
+else:
+    print(os.path.abspath(filename), "already exists!") # create a custom error here
 
